3839371e8f51f21264d569000b70ba9dc9f7fa2e
leacent <leason316@foxmail.com>
2024-04-30 20:54:14 +0800

    feat: init rusd graph

--- stake-rusd-manage/src/rusd-stake-manager.ts
+++ stake-rusd-manage/src/rusd-stake-manager.ts
@@ -9,8 +9,8 @@ import {
   SetOutUSDBVault as SetOutUSDBVaultEvent,
   StakeRUSD as StakeRUSDEvent,
   Unstake as UnstakeEvent,
-  WithdrawYield as WithdrawYieldEvent
-} from "../generated/RUSDStakeManager/RUSDStakeManager"
+  WithdrawYield as WithdrawYieldEvent,
+} from "../generated/RUSDStakeManager/RUSDStakeManager";
 import {
   ClaimMaxGas,
   ExtendLockTime,
@@ -22,37 +22,21 @@ import {
   SetOutUSDBVault,
   StakeRUSD,
   Unstake,
-  WithdrawYield
-} from "../generated/schema"
+  WithdrawYield,
+} from "../generated/schema";
 
 export function handleClaimMaxGas(event: ClaimMaxGasEvent): void {
   let entity = new ClaimMaxGas(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.recipient = event.params.recipient
-  entity.gasAmount = event.params.gasAmount
+  );
+  entity.recipient = event.params.recipient;
+  entity.gasAmount = event.params.gasAmount;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
-}
-
-export function handleExtendLockTime(event: ExtendLockTimeEvent): void {
-  let entity = new ExtendLockTime(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.positionId = event.params.positionId
-  entity.extendDays = event.params.extendDays
-  entity.newDeadLine = event.params.newDeadLine
-  entity.mintedRUY = event.params.mintedRUY
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  entity.save();
 }
 
 export function handleGasManagerTransferred(
@@ -60,15 +44,15 @@ export function handleGasManagerTransferred(
 ): void {
   let entity = new GasManagerTransferred(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousGasManager = event.params.previousGasManager
-  entity.newGasManager = event.params.newGasManager
+  );
+  entity.previousGasManager = event.params.previousGasManager;
+  entity.newGasManager = event.params.newGasManager;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleOwnershipTransferred(
@@ -76,114 +60,135 @@ export function handleOwnershipTransferred(
 ): void {
   let entity = new OwnershipTransferred(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+  );
+  entity.previousOwner = event.params.previousOwner;
+  entity.newOwner = event.params.newOwner;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleSetForceUnstakeFee(event: SetForceUnstakeFeeEvent): void {
   let entity = new SetForceUnstakeFee(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.forceUnstakeFee = event.params.forceUnstakeFee
+  );
+  entity.forceUnstakeFee = event.params.forceUnstakeFee;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleSetMaxLockupDays(event: SetMaxLockupDaysEvent): void {
   let entity = new SetMaxLockupDays(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.maxLockupDays = event.params.maxLockupDays
+  );
+  entity.maxLockupDays = event.params.maxLockupDays;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleSetMinLockupDays(event: SetMinLockupDaysEvent): void {
   let entity = new SetMinLockupDays(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.minLockupDays = event.params.minLockupDays
+  );
+  entity.minLockupDays = event.params.minLockupDays;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleSetOutUSDBVault(event: SetOutUSDBVaultEvent): void {
   let entity = new SetOutUSDBVault(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.outUSDBVault = event.params.outUSDBVault
+  );
+  entity.outUSDBVault = event.params.outUSDBVault;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
+}
+
+export function handleExtendLockTime(event: ExtendLockTimeEvent): void {
+  let entity = new ExtendLockTime(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+
+  let stakeEntity = StakeRUSD.load(event.params.positionId.toString());
+  if (stakeEntity !== null) {
+    stakeEntity.deadline = event.params.newDeadLine;
+    stakeEntity.save();
+  }
+
+  entity.positionId = event.params.positionId;
+  entity.extendDays = event.params.extendDays;
+  entity.newDeadLine = event.params.newDeadLine;
+  entity.mintedRUY = event.params.mintedRUY;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
 }
 
 export function handleStakeRUSD(event: StakeRUSDEvent): void {
-  let entity = new StakeRUSD(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.positionId = event.params.positionId
-  entity.account = event.params.account
-  entity.amountInRUSD = event.params.amountInRUSD
-  entity.amountInPUSD = event.params.amountInPUSD
-  entity.amountInRUY = event.params.amountInRUY
-  entity.deadline = event.params.deadline
+  let entity = new StakeRUSD(event.params.positionId.toString());
+  entity.positionId = event.params.positionId;
+  entity.account = event.params.account;
+  entity.amountInRUSD = event.params.amountInRUSD;
+  entity.amountInPUSD = event.params.amountInPUSD;
+  entity.amountInRUY = event.params.amountInRUY;
+  entity.deadline = event.params.deadline;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleUnstake(event: UnstakeEvent): void {
   let entity = new Unstake(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.positionId = event.params.positionId
-  entity.amountInRUSD = event.params.amountInRUSD
-  entity.burnedPUSD = event.params.burnedPUSD
-  entity.burnedRUY = event.params.burnedRUY
+  );
+  entity.positionId = event.params.positionId;
+  entity.amountInRUSD = event.params.amountInRUSD;
+  entity.burnedPUSD = event.params.burnedPUSD;
+  entity.burnedRUY = event.params.burnedRUY;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleWithdrawYield(event: WithdrawYieldEvent): void {
   let entity = new WithdrawYield(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-  entity.burnedRUY = event.params.burnedRUY
-  entity.yieldAmount = event.params.yieldAmount
+  );
+  entity.account = event.params.account;
+  entity.burnedRUY = event.params.burnedRUY;
+  entity.yieldAmount = event.params.yieldAmount;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
